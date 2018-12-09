bb3b345939befb2dba6d8b27c6fea92b8cb65859
Tadas Talaikis <xenu256@users.noreply.github.com>
2018-12-09 16:48:46 +0200

    init

--- src/env.js
+++ src/env.js
@@ -1,9 +1,10 @@
-export const GA = 'UA-120129778-1'
-
-export const DECIMALS = 18
-
-export const  TOKEN_NAME = 'BLUE'
-
-export const API_URL = process.env.NODE_ENV === 'production' 
+const env = {
+  GA: 'UA-120129778-1',
+  DECIMALS: 18,
+  TOKEN_NAME: 'BLUE',
+  API_URL: process.env.NODE_ENV === 'production' 
   ? 'https://api.blueblood.ltd'
   : 'http://localhost:8080'
+}
+
+export default env
